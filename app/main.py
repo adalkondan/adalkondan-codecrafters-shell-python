639a4c05c05b51963210f2658707f472975e8360
adalkondan <01101998skk@gmail.com>
2025-02-03 18:43:33 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,12 +4,17 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     #sys.stdout.write("$ ")
-
+    def echo(message):
+        print(" ".join(message))
     # Wait for user input
     while(True):
         
         sys.stdout.write("$ ")
-        command=input()
+        command=input().split()
+        user_input = command[0]
+        messag = command[1:]
+        if command=="echo":
+            echo(messag)
         if command=="exit 0":
             break
         print(f"{command}: command not found")
