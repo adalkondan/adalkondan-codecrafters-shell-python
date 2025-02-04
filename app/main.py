68dbd80c46060b22fb443bbfd21286a578c2e0cb
adalkondan <01101998skk@gmail.com>
2025-02-04 19:02:56 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ def main():
           if executable_path:
             try:
                 # Run the external command with its arguments
-                result = subprocess.run([user_input]+messag,env=env, capture_output=False,text=True)
+                result = subprocess.run([user_input]+messag, capture_output=False,text=True)
             except subprocess.CalledProcessError:
                 # Handle error if command execution fails
                 print(f"Error executing {user_input}")
