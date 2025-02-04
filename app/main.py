af682724d425f8d7976ff25a58e7ea15cc6b4818
adalkondan <01101998skk@gmail.com>
2025-02-04 18:31:48 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 import sys
+import os
 
 
 def main():
     # Uncomment this block to pass the first stage
     #sys.stdout.write("$ ")
+    def find_executable(executable):
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            executable_path = os.path.join(path, executable)
+            if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
+                return executable_path
+        return None
     def echo(messag):
         return print(" ".join(messag))
     # Wait for user input\
@@ -11,8 +19,14 @@ def main():
         builtins = ['echo', 'exit', 'type']
         if messag[0] in builtins:
             print(f"{messag[0]} is a shell builtin")
+            return
+        executable_path = find_executable(messag[0])
+        
+        if executable_path:
+            print(f"{messag[0]} is {executable_path}")
         else:
             print(f"{messag[0]}: not found")
+        
     while(True):
         
         sys.stdout.write("$ ")
