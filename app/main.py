4652223215f9b0af8a09bd2321b96f897c708cb5
adalkondan <01101998skk@gmail.com>
2025-02-04 19:45:31 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -2,10 +2,7 @@ import sys
 import os
 import subprocess
 
-
 def main():
-    # Uncomment this block to pass the first stage
-    #sys.stdout.write("$ ")
     def find_executable(executable):
         for path in os.environ["PATH"].split(os.pathsep):
             path = path.strip('"')
@@ -13,47 +10,44 @@ def main():
             if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
                 return executable_path
         return None
+
     def echo(messag):
         return print(" ".join(messag))
-    # Wait for user input\
+
     def type(messag):
         builtins = ['echo', 'exit', 'type']
         if messag[0] in builtins:
             print(f"{messag[0]} is a shell builtin")
             return
         executable_path = find_executable(messag[0])
-        
         if executable_path:
             print(f"{messag[0]} is {executable_path}")
         else:
             print(f"{messag[0]}: not found")
-        
-    while(True):
-        
+
+    while True:
         sys.stdout.write("$ ")
-        command=input().split()
+        sys.stdout.flush()
+        command = input().split()
+        if not command:
+            continue
         user_input = command[0]
         messag = command[1:]
-        if command[0]=="echo":
+        if user_input == "echo":
             echo(messag)
-        elif user_input== "exit":
+        elif user_input == "exit":
             break
-        elif command[0]=="type":
+        elif user_input == "type":
             type(messag)
         else:
-          executable_path = find_executable(user_input)
-          if executable_path:
-            try:
-                # Run the external command with its arguments
-                # full_command = [user_input] + messag
-                subprocess.run([executable_path]+messag, capture_output=False,text=True)
-            except subprocess.CalledProcessError:
-                # Handle error if command execution fails
-                print(f"Error executing {user_input}")
+            executable_path = find_executable(user_input)
+            if executable_path:
+                try:
+                    subprocess.run([executable_path] + messag, capture_output=False, text=True)
+                except subprocess.CalledProcessError:
+                    print(f"Error executing {user_input}")
             else:
                 print(f"{user_input}: command not found")
-    
-
 
 if __name__ == "__main__":
     main()
