15dd7aef1e018f74098b3e0d59d327efdb6c06c3
adalkondan <01101998skk@gmail.com>
2025-02-06 20:16:57 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,96 +1,95 @@
-import sys
 import os
-import subprocess
-import re
-import shlex
-
-
-def main():
-    def find_executable(executable):
-        for path in os.environ["PATH"].split(os.pathsep):
-            path = path.strip('"')
-            executable_path = os.path.join(path, executable)
-            if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
-                return executable_path
-        return None
-
-    def echo(messag):
-        cleaned_msg = []
-        for word in messag:
-          # cleaned_word = re.sub(r"'([^']*)'", r"\1", word)
-          # cleaned_word = re.sub(r'"([^"]*)"', r"\1", cleaned_word)
-          cleaned_word = re.sub(r"'([^']*)'", lambda m: m.group(1).replace("\\\\", "\\").replace("\\'", "'").replace(" ",""), word)
-          # cleaned_word = re.sub(r"'([^']*)'|\"([^']*)\"", lambda m: m.group(1) if m.group(1) is not None else m.group(2).replace("\\\\", "\\").replace('\\"', '"'), word)
-          # Append the cleaned word
-          cleaned_msg.append(cleaned_word)
-
-        print(" ".join(cleaned_msg))
-
-    def type(messag):
-        builtins = ['echo', 'exit', 'type','pwd']
-        if messag[0] in builtins:
-            print(f"{messag[0]} is a shell builtin")
-            return
-        executable_path = find_executable(messag[0])
-        if executable_path:
-            print(f"{messag[0]} is {executable_path}")
-        else:
-            print(f"{messag[0]}: not found")
-    def cd(messag):
-        if messag [0] == "~":
-            os.chdir(os.path.expanduser("~"))
-            return
-        try:
-            os.chdir(messag[0])
-        except FileNotFoundError:
-            print(f"cd: {messag[0]}: No such file or directory")
-        # except NotADirectoryError:
-        #     print(f"cd: {messag[0]}: Not a directory")
-        # except PermissionError:
-        #     print(f"cd: {messag[0]}: Permission denied")
-    def cat(messag):
-            for file_p in messag:
-                try:
-                    process_p = re.sub(r'[^a-zA-Z0-9/]', '', file_p)
-                    with open(process_p, "r") as file:
-                        print(process_p.read(),end=" ")
-                except FileNotFoundError:
-                    process_p = file_p.replace("'","").replace("\'","")
-                    with open(process_p, "r") as file:
-                        print(process_p.read(),end=" ")
-                    # print(f"cat: {process_p}: No such file or directory")
-                    # print(process_p.read(),end=" ")
-
-    while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
-        command = input().strip()
-        command = shlex.split(command, posix=True)
-        if not command:
-            continue
-        user_input = command[0]
-        messag = command[1:]
-        if user_input == "echo":
-            echo(messag)
-        elif user_input == "exit":
-            break
-        elif user_input == "type":
-            type(messag)
-        elif user_input == "cd":
-            cd(messag)
-        elif user_input == "pwd":
-            print(os.getcwd())
-        elif user_input == "cat":
-            cat(messag)
-        else:
-            executable_path = find_executable(user_input)
-            if executable_path:
-                try:
-                    subprocess.run([user_input] + messag,executable=executable_path, capture_output=False, text=True)
-                except subprocess.CalledProcessError:
-                    print(f"Error executing {user_input}")
+import sys
+class command:
+    def __init__(this, name, func, sh_type):
+        this.name = name
+        this.func = func
+        this.sh_type = sh_type
+    def execute(this, argc, argv):
+        argv = pars_args(argv)
+        this.func(*argv)
+def pars_args(argv):
+    s_quote = False
+    d_quote = False
+    quote_buffer = ""
+    quote_index = 0
+    args_buffer = []
+    for i, arg in enumerate(argv):
+        backslash = find(arg, "\\")
+        if arg.startswith('"'):
+            if arg.endswith('"'):
+                quote_buffer = ""
+                args_buffer.append(arg[1:-1])
+                continue
+            d_quote = True
+            quote_buffer += arg[1:]
+            quote_index = i
+        elif d_quote == True:
+            if arg.endswith('"'):
+                d_quote = False
+                quote_buffer += " " + arg[:-1]
+                args_buffer.append(quote_buffer)
+                quote_buffer = ""
             else:
-                print(f"{user_input}: command not found")
-
-if __name__ == "__main__":
-    main()
+                quote_buffer += " " + arg
+        elif arg.startswith("'") and d_quote == False:
+            if arg.endswith("'"):
+                quote_buffer = ""
+                args_buffer.append(arg[1:-1])
+                continue
+            s_quote = True
+            quote_buffer += arg[1:]
+            quote_index = i
+        elif s_quote == True:
+            if arg.endswith("'"):
+                s_quote = False
+                quote_buffer += " " + arg[:-1]
+                args_buffer.append(quote_buffer)
+                quote_buffer = ""
+            else:
+                quote_buffer += " " + arg
+        else:
+            if d_quote == False and s_quote == False:
+                if len(backslash) != 0:
+                    start_idx = 0
+                    buffer = ""
+                    add_end = True
+                    for idx in backslash:
+                        if len(arg) == (idx + 1):
+                            buffer += arg[:-1] + " "
+                            add_end = False
+                        else:
+                            buffer += arg[start_idx:idx]
+                            start_idx = idx + 1
+                    if add_end:
+                        buffer += arg[start_idx:]
+                    args_buffer.append(buffer)
+                else:
+                    args_buffer.append(arg)
+            else:
+                args_buffer.append(arg)
+    if s_quote == True or d_quote == True:
+        sys.stdout.write(f"Error parsing quote string")
+    argv = args_buffer
+    return argv
+class shell_type:
+    def __init__(this, name):
+        this.name = name
+BuiltIn = shell_type("shell builtin")
+Types = {"builtin": BuiltIn}
+def find(s, ch):
+    return [i for i, ltr in enumerate(s) if ltr == ch]
+def search_path(name):
+    PATH_ENV = os.environ.get("PATH")
+    path_dirs = PATH_ENV.split(":")
+    commands = []
+    for path_dir in path_dirs:
+        try:
+            files = os.listdir(path_dir)
+        except FileNotFoundError:
+            pass  # this means there is no directory for that path entry
+        files = [file for file in files if os.path.isfile(f"{path_dir}/{file}")]
+        for file in files:
+            if file == name:
+                return f"{path_dir}/{file}"
+    return None
